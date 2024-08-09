fd06d3cc1554e3eb93387f7d346730dbd37de4aa
siva Ranjani <sivaranjani@sivas-MacBook-Air.local>
2024-08-09 17:05:53 -0400

    Updated two files for Data loading and Image processing

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,16 +1,12 @@
 import tensorflow as tf
 
-
 def preprocess_image(image, label):
     image = tf.image.resize(image, [64, 64])
     image = tf.cast(image, tf.float32) / 255.0
     image = tf.expand_dims(image, -1)  # Ensure image has a single channel
     return image, label
 
-
 def prepare_datasets(train_data, test_data):
-    train_data = train_data.map(preprocess_image).cache().shuffle(
-        10000).batch(32).prefetch(tf.data.AUTOTUNE)
-    test_data = test_data.map(preprocess_image).cache().batch(
-        32).prefetch(tf.data.AUTOTUNE)
+    train_data = train_data.map(preprocess_image).cache().shuffle(10000).batch(32).prefetch(tf.data.AUTOTUNE)
+    test_data = test_data.map(preprocess_image).cache().batch(32).prefetch(tf.data.AUTOTUNE)
     return train_data, test_data
