a38f3e6571097959700a589c439eeefd6d29487b
Jathursha Patpanathan <jathursha.patpanathan@dcmail.ca>
2024-08-09 20:04:37 -0400

    star recognitiion and main files updated

--- src/main.py
+++ src/main.py
@@ -1,35 +1,41 @@
+from star_recognition import recognize_stars, label_stars
+from image_processing import preprocess_image
+from data_loader import load_dataset
 import sys
 import os
 import matplotlib.pyplot as plt
-import tensorflow as tf
-import numpy as np
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+# Ensure the src directory is included in the module search path
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', 'src')))
 
-from data_loader import load_dataset
-from image_processing import preprocess_image
-from star_recognition import recognize_stars, label_stars
 
 def main():
-    dataset, _ = load_dataset()
+    # Load the dataset and info
+    dataset, _ = load_dataset()  # Dataset is the first element
+
+    # Get an image from the dataset
     image, _ = next(iter(dataset))
 
-    # Convert TensorFlow tensor to NumPy array if needed
-    if isinstance(image, tf.Tensor):
-        image = image.numpy()
-
+    # Check image shape
     print(f"Image shape: {image.shape}")
 
-    star_label = recognize_stars(image)
-    labeled_image = label_stars(image, star_label)
+    # Recognize stars in the image
+    star_label = recognize_stars(image)  # Adjust as needed
 
+    # Label the stars in the image
+    labeled_image = label_stars(image, star_label)  # Adjust as needed
+
+    # Check the shape of labeled_image and reshape if needed
     if len(labeled_image.shape) == 4:
-        labeled_image = np.squeeze(labeled_image)
+        labeled_image = labeled_image.squeeze()
 
+    # Display the image using matplotlib
     plt.imshow(labeled_image, cmap='gray')
     plt.title('Labeled Stars')
-    plt.axis('off')
+    plt.axis('off')  # Hide axes
     plt.show()
+
 
 if __name__ == "__main__":
     main()
